28667ddc4b8171e366304dace795750666bf738a
Herman Fries <h.fries@nyris.io>
2019-11-26 12:02:52 +0100

    Add example settings file

---
+++ public/js/settings.example.js
@@ -0,0 +1,28 @@
+
+var settings = {
+  "apiKey": "xxx",
+  "maxWidth": 500,
+  "maxHeight": 500,
+  "jpegQuality": 0.9,
+  "regions": true,
+  "preview": true,
+  "xOptions": "",
+  "imageMatchingUrl": "http://api.nyris.io/find/v1/",
+  "imageMatchingUrlBySku": "https://api.nyris.io/recommend/v1/",
+  "imageMatchingSubmitManualUrl": "https://api.nyris.io/find/v1/manual/",
+  "regionProposalUrl": "https://api.nyris.io/find/v1/regions/",
+  "feedbackUrl": "https://api.nyris.io/feedback/v1/",
+  "exampleImages": [
+    "https://img.nyris.io/demo/everybag/kissen.jpg",
+    "https://img.nyris.io/demo/everybag/aspirin.jpg",
+    "https://img.nyris.io/demo/everybag/lego.jpg",
+    "https://img.nyris.io/demo/everybag/wdr_add_2.jpg",
+    "https://img.nyris.io/demo/everybag/mb-dle-4.jpg",
+    "https://img.nyris.io/demo/everybag/1.jpg",
+    "https://img.nyris.io/demo/everybag/5.jpg",
+    "https://img.nyris.io/demo/everybag/6.jpg"
+  ]
+};
+settings["customSearchRequest"] = null;
+settings["responseHook"] =  null;
+
