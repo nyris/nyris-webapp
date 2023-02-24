4c0959fc94e6ee6bbaa63cb4e30bc148e9cdf544
Ridwan <ridwan.bin.sarwar@gmail.com>
2023-02-24 15:29:21 +0600

    changed recognition_image_link to additional_image_link

--- packages/nyris-webapp/src/helpers/CommonHelper.ts
+++ packages/nyris-webapp/src/helpers/CommonHelper.ts
@@ -1,27 +1,27 @@
-import {isEmpty} from "lodash";
+import { isEmpty } from 'lodash';
 
 export const prepareImageList = (dataItem: any) => {
-    let valueKey = [];
-    const newObject = dataItem;
+  let valueKey = [];
+  const newObject = dataItem;
 
-    if (!dataItem) {
-        return [];
-    }
+  if (!dataItem) {
+    return [];
+  }
 
-    for (let key in newObject) {
-        if (key?.includes('recognition_image_link')) {
-            if (!isEmpty(newObject[key])) {
-                valueKey.push({
-                    url: newObject[key],
-                });
-            }
-        } else {
-            if (key === 'main_image_link') {
-                valueKey.push({
-                    url: newObject[key],
-                });
-            }
-        }
+  for (let key in newObject) {
+    if (key?.includes('additional_image_link')) {
+      if (!isEmpty(newObject[key])) {
+        valueKey.push({
+          url: newObject[key],
+        });
+      }
+    } else {
+      if (key === 'main_image_link') {
+        valueKey.push({
+          url: newObject[key],
+        });
+      }
     }
-    return valueKey;
+  }
+  return valueKey;
 };
