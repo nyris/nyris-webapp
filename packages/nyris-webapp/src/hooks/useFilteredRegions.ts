fabd5a390815311e122fd2426ba418b89e9c4dc8
Ridwan <ridwan.bin.sarwar@gmail.com>
2023-08-28 18:42:05 +0600

    Fix app crash when changing screen

--- packages/nyris-webapp/src/hooks/useFilteredRegions.ts
+++ packages/nyris-webapp/src/hooks/useFilteredRegions.ts
@@ -8,18 +8,18 @@ const useFilteredRegions = (regions: any, imageSelection: any) => {
           normalizedRect: { x1: any; x2: any; y1: any; y2: any };
         }) => {
           if (
-            region.normalizedRect.x1 === imageSelection.x1 &&
-            region.normalizedRect.x2 === imageSelection.x2 &&
-            region.normalizedRect.y1 === imageSelection.y1 &&
-            region.normalizedRect.y2 === imageSelection.y2
+            region.normalizedRect?.x1 === imageSelection?.x1 &&
+            region.normalizedRect?.x2 === imageSelection?.x2 &&
+            region.normalizedRect?.y1 === imageSelection?.y1 &&
+            region.normalizedRect?.y2 === imageSelection?.y2
           ) {
             return { ...region, show: false };
           }
           if (
-            imageSelection.x1 === 0 &&
-            imageSelection.x2 === 1 &&
-            imageSelection.y1 === 0 &&
-            imageSelection.y2 === 1
+            imageSelection?.x1 === 0 &&
+            imageSelection?.x2 === 1 &&
+            imageSelection?.y1 === 0 &&
+            imageSelection?.y2 === 1
           ) {
             return { ...region, show: false };
           }
