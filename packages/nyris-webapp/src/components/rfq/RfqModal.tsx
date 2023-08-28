7f5d2769433acd20e15bcece693fbf2b9d8d353a
Ridwan <ridwan.bin.sarwar@gmail.com>
2023-08-28 19:42:40 +0600

    Add rfq request sent failed toast

--- packages/nyris-webapp/src/components/rfq/RfqModal.tsx
+++ packages/nyris-webapp/src/components/rfq/RfqModal.tsx
@@ -6,7 +6,8 @@ import emailjs from '@emailjs/browser';
 import { ToastHelper } from 'helpers/ToastHelper';
 import { isUndefined } from 'lodash';
 import { TextareaAutosize } from '@material-ui/core';
-
+import toast from 'react-hot-toast';
+import { ReactComponent as ErrorIcon } from 'common/assets/icons/error.svg';
 interface Props {
   requestImage: any;
   selectedRegion: any;
@@ -52,7 +53,53 @@ export default function RfqModal({
       ToastHelper.success('Request sent successfully');
     } catch (error) {
       setRfqStatus('inactive');
-      ToastHelper.error('Request sent failed');
+      toast(
+        t => {
+          return (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                fontSize: '14px',
+                width: '294px',
+              }}
+            >
+              <span style={{ fontWeight: 'bold' }}>Email not sent</span>
+              <span>
+                Your email could not be sent, please try again or send an email
+                to support@nyris.io
+              </span>
+              <a
+                href={`mailto:support@nyris.io?subject=Request for quotation&body= Hello, please provide details for the following product.`}
+                style={{
+                  padding: '8px 16px 8px 16px',
+                  border: '1px solid #000',
+                  marginTop: '16px',
+                  backgroundColor: 'transparent',
+                  color: '#000',
+                  cursor: 'pointer',
+                  width: 'fit-content',
+                }}
+              >
+                support@nyris.io
+              </a>
+            </div>
+          );
+        },
+        {
+          duration: 5000,
+          style: {
+            background: '#FFE5EF',
+            color: '#000000',
+            maxWidth: '400px',
+          },
+          icon: (
+            <div style={{ minWidth: '20px', minHeight: '20px' }}>
+              <ErrorIcon />
+            </div>
+          ),
+        },
+      );
     }
     setIsRfqModalOpen(false);
   };
