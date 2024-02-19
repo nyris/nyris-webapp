0e1dc65f6a983bcdeee46f76ce72f8ebe9137fb0
Ridwan <r.sarwar@nyris.io>
2024-02-19 18:08:13 +0600

    NYRIS-6145: update settings example

--- packages/nyris-webapp/public/js/settings.example.js
+++ packages/nyris-webapp/public/js/settings.example.js
@@ -1,19 +1,90 @@
 var settings = {
-  apiKey: 'xxx',
-  maxWidth: 500,
-  maxHeight: 500,
-  jpegQuality: 0.9,
-  regions: true,
-  preview: true,
-  baseUrl: 'https://api.nyris.io',
-  xOptions: '',
-  instantRedirectPatterns: [
-    '^https?://(www.)?youtube.com/',
-    '^https?://(www.)?youtu.be/',
-    '^https?://(www.)?vimeo.com/',
-    '^https?://(www.)?dailymotion.com/',
-    '^https?://(www.)?dai.ly/',
+  auth0: {
+    enabled: '',
+    domain: '',
+    clientId: '',
+    supportEmail: '',
+  },
+  algolia: {
+    apiKey: '',
+    appId: '',
+    indexName: '',
+  },
+  alogoliaFilterField: 'keyword',
+  refinements: [
+    {
+      attribute: 'brand',
+      header: 'Brands',
+    },
+    {
+      attribute: 'keyword_0',
+      header: 'Category',
+    },
   ],
+  // Nyris - visual search
+  apiKey: '',
+  baseUrl: 'https://api.nyris.io',
+  jpegQuality: 0.9,
+  maxHeight: 1024,
+  maxWidth: 1024,
+  regions: true,
+  responseFormat: 'application/offers.complete+json',
+  visualSearchFilterKey: '',
+  // UI - theme
+  theme: {
+    appBarLogoUrl: '',
+    headerColor: '',
+    logoHeight: '',
+    logoWidth: '',
+    primaryColor: '',
+    secondaryColor: '',
+  },
+  language: 'en',
+  // features
+  shareOption: '',
+  showFeedbackAndShare: '',
+  showPoweredByNyris: '',
+  warehouseVariant: false,
+  postFilterOption: '',
+  preFilterOption: '',
+  rfq: {
+    enabled: '',
+    emailTemplateId: '',
+  },
+  support: {
+    enabled: '',
+    emailInquiry: '',
+    emailTemplateId: '',
+    supportNumber: '',
+    description: '',
+  },
+  cadenas: {
+    catalog: '',
+    cadenasAPIKey: '',
+    cadenas3dWebView: '',
+  },
+  //UI - labels
+  appTitle: '',
+  brandName: '',
+  headerText: '',
+  itemIdLabel: 'SKU',
+  preFilterTitle: '',
+  CTAButtonText: '',
+  //field mappins
+  field: {
+    ctaLinkField: 'main_offer_link',
+    productName: 'title',
+    productDetails: 'keyword_1',
+    manufacturerNumber: 'keyword_0',
+    productTag: 'brand',
+    //WarehouseVariant
+    warehouseNumber: 'custom_id_key_2',
+    warehouseNumberValue: 'custom_id_value_2',
+    warehouseShelfNumber: 'custom_id_key_1',
+    warehouseShelfNumberValue: 'custom_id_value_1',
+    warehouseStock: 'custom_id_key_3',
+    warehouseStockValue: 'custom_id_value_3',
+  },
 };
 settings['customSearchRequest'] = null;
 settings['responseHook'] = null;
