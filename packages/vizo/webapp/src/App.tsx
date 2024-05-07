24c67ef9a0208f18d278e13ed85082e858c38a22
romankrasko <r.krasko@nyris.io>
2024-05-07 14:17:10 +0200

    added drag and drop

--- packages/vizo/webapp/src/App.tsx
+++ packages/vizo/webapp/src/App.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import drop_zone from "./assets/dropzone.svg";
 
-function App() {
-  return <div>Vizo</div>;
+interface IAppProps {
+  search: any;
+}
+
+function App(props: IAppProps) {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: (fs: File[]) => onFileDropped(fs[0]),
+  });
+
+  const onFileDropped = (file: any) => {
+    console.log(file);
+  }
+  return (
+    <>
+      <div
+        className={`drag-n-drop ${
+          isDragActive ? "active-drop" : ""
+        }`}
+        {...getRootProps()}
+      >
+        <img src={drop_zone} width={48} height={48} />
+        <div className="drag-n-drop-text">
+          <strong>Drag and drop</strong> an image here
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
--- packages/vizo/webapp/src/Layout.tsx
+++ packages/vizo/webapp/src/Layout.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './App.scss';
 import { ReactComponent as CloseIcon } from './assets/close.svg';
+import drop_zone from './assets/dropzone.svg';
 interface ILayoutProps {
   children: any;
 }
 function Layout(props: ILayoutProps) {
   const [searchKey, setSearchKey] = useState<string>('');
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: (fs: File[]) => onFileDropped(fs[0]),
+  });
+
+  const onFileDropped = (file: any) => {
+    console.log(file);
+  }
+
   return (
     <div className="layout">
       <header>
@@ -18,29 +28,41 @@ function Layout(props: ILayoutProps) {
       </header>
       <main>
         <div className="search-bar">
-          <input
-            className="search-bar-input"
-            type="text"
-            onChange={(e) => setSearchKey(e.target.value)}
-            value={searchKey}
-          />
-          {searchKey
-            ? <CloseIcon
+          <div className="ask-vizo">
+            Ask Vizo!
+          </div>
+          <div className="text-search-bar">
+            <input
+              className="text-search-bar-input"
+              type="text"
+              onChange={(e) => setSearchKey(e.target.value)}
+              value={searchKey}
+              placeholder="Search"
+            />
+            {searchKey
+              ? <CloseIcon
                 className="clear-icon"
                 width={16}
                 height={16}
                 fill="#2B2C46"
                 onClick={() => setSearchKey('')}
-            />
-            : ''
-          }
+              />
+              : ''
+            }
+          </div>
         </div>
         <div>
           {props.children}
         </div>
       </main>
       <footer>
-        <a href={'https://www.nyris.io'} target="_blank">Powered by <strong>nyris</strong></a>
+        <a
+          href={'https://www.nyris.io'}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Powered by <strong>nyris®</strong>
+        </a>
       </footer>
     </div>
   )
