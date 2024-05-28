1f6aa32cb45a88018266e3f9fe350b4056d8e475
romankrasko <r.krasko@nyris.io>
2024-05-28 16:35:57 +0200

    Fix for scrolls and logout

--- packages/vizo/webapp/src/Router.tsx
+++ packages/vizo/webapp/src/Router.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import Layout from './Layout';
 import Login from './components/Login';
 import Logout from './components/Logout';
 
 
 function Router(): JSX.Element {
+  const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !user && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [loginWithRedirect, isLoading, user, isAuthenticated]);
+
   return (
     <Switch>
       <Switch>
