c982a6c37e313da1f18321e205a53bd8ac602206
romankrasko <r.krasko@nyris.io>
2024-07-04 15:01:45 +0200

    Fix for prefilter indicator

--- packages/nyris-webapp/src/components/HeaderMobile.tsx
+++ packages/nyris-webapp/src/components/HeaderMobile.tsx
@@ -320,7 +320,7 @@ function HeaderMobileComponent(props: Props): JSX.Element {
                     </div>
                   )}
                   {!showPreFilter && <IconSearch width={16} height={16} />}
-                  {!isEmpty(preFilter) && (
+                  {!isEmpty(preFilter) && settings.preFilterOption && (
                     <div
                       style={{
                         position: 'absolute',
