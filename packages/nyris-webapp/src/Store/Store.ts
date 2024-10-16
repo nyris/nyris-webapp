422fba9efa3d6344927cb454240f44eb3433c7d9
Ridwan <r.sarwar@nyris.io>
2024-10-16 21:56:35 +0600

    Nt: Remove  showFeedback always disabled

--- packages/nyris-webapp/src/Store/Store.ts
+++ packages/nyris-webapp/src/Store/Store.ts
@@ -12,7 +12,6 @@ settings.algolia.enabled = true;
 settings.preview = true;
 settings.showFeedbackAndShare = false;
 settings.multiImageSearch = false;
-settings.showFeedback = false;
 
 if (settings.rfq?.enabled && settings.support?.enabled) {
   settings.support.enabled = false;
